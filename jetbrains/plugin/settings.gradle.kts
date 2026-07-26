37c629809deafc589ca29f8a7c957695d4f6dbef
homremwork <romka0075@yandex.by>
2026-07-26 20:49:10 +0300

    build(jetbrains): align Kotlin toolchain with IntelliJ 2025.3

--- jetbrains/plugin/settings.gradle.kts
+++ jetbrains/plugin/settings.gradle.kts
@@ -2,4 +2,18 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                // IntelliJ Platform 2025.3 bundles Kotlin 2.2.20 metadata.
+                "org.jetbrains.kotlin.jvm" -> useVersion("2.2.20")
+
+                // 2025.3 unified IntelliJ distributions require 2.10.4 or newer.
+                "org.jetbrains.intellij.platform" -> useVersion("2.10.4")
+            }
+        }
+    }
+}
+
 rootProject.name = "Kilo Code"
